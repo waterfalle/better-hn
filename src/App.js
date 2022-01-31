1a6a101da52549219998f111e52562ddc10b1309
Max <fake>
2022-02-01 00:18:47 +1100

    implemented code so that the top 500 stories are retrieved and displayed by using the hacker news api. However, the speed is very slow and it takes a while to retrieve the list of stories.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-function App() {
+const storiesGet = async () => {
+  const url = "https://hacker-news.firebaseio.com/v0/topstories.json";
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    const storyList = await Promise.all(data.map((itemID) => storyItemGet(itemID).then(res => res)));
+    // gets an array of promises.
+    console.log(storyList)
+    return storyList;
+  } catch (e) {
+    console.error("ERROR", e);
+  }
+};
+
+const storyItemGet = async (itemID) => {
+  const url = `https://hacker-news.firebaseio.com/v0/item/${itemID}.json`
+  try {
+    const resp = await fetch(url);
+    const storyItem = await resp.json();
+    return storyItem;
+  } catch {
+    console.error("ERROR: Can't get story item");
+  }
+};
+
+const List = ({data}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ol>
+      {data.map((story) => (
+        <li key={story.id}>
+          <a href={story.url} target='_blank' rel="noreferrer">
+            {story.title}
+          </a>
+          <br></br>
+          <span>Score:    {story.score}</span>
+          <br></br>
+          <span>Author:   {story.by}</span>
+          <hr></hr>
+        </li>
+      ))}
+    </ol>
   );
 }
+
+const App = () => {
+  const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsLoading(true);
+    storiesGet().then(resp => {
+      setStories(resp);
+      setIsLoading(false);
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>Hello World! Top 500 Hacker News Stories</h1>
+      {isLoading ? <p>Loading stories, please wait...</p> : <List data={stories}/>}
+      {/* the ternary operator will show a loading screen if isLoading is true,
+      otherwise it will show the list of stories */}
+    </>
+  );
+};
 
 export default App;
